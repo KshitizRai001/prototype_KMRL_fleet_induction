d798ee33a98cd943fb0fffa2c1f588009c9aa34a
Kshitiz Rai <24cd3023@rgipt.ac.in>
2025-09-29 19:58:11 +0530

    warp 2

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -3,7 +3,3 @@ import { createServer } from "../../server";
 
 // Wrap the existing Express app as a Netlify Function
 export const handler = serverless(createServer());
-
-import { createServer } from "../../server";
-
-export const handler = serverless(createServer());
