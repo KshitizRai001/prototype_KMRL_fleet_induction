e469643f91f89996068c4afd3b97b195bd402375
Kshitiz Rai <24cd3023@rgipt.ac.in>
2025-09-29 13:50:48 +0530

    Integrate PostgreSQL and fix database connection

--- kmrl_backend/kmrl_backend/settings.py
+++ kmrl_backend/kmrl_backend/settings.py
@@ -84,11 +84,11 @@ WSGI_APPLICATION = 'kmrl_backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('PGDATABASE'),
-        'USER': os.environ.get('PGUSER'),
-        'PASSWORD': os.environ.get('PGPASSWORD'),
-        'HOST': os.environ.get('PGHOST'),
-        'PORT': os.environ.get('PGPORT', '5432'),
+        'NAME': 'kmrl_db',
+        'USER': 'postgres',
+        'PASSWORD': 'sixstrings@1',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
